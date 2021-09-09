70c9585b022e8ce2677f69398bd1bc97f42b29a1
Cassiano Sales <cajose-s@student.42sp.org.br>
2021-09-09 14:49:04 -0300

    ft_striteri atualizada

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	*ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int i;
 
